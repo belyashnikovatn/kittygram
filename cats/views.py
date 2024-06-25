3de4c74918a02a58449f3ddbe8cf8e4c15b175a1
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-06-25 09:09:45 +0500

    change statuses

--- cats/views.py
+++ cats/views.py
@@ -23,13 +23,13 @@ def cat_list(request):
 def cat_detail(request, pk):
     cat = Cat.objects.get(pk=pk)
     if request.method == 'PUT' or request.method == 'PATCH':
-        serializer = CatSerializer(cat, data=request.data, partial = True)
+        serializer = CatSerializer(cat, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         cat.delete()
-        return Response(status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_204_NO_CONTENT)
     serializer = CatSerializer(cat)
     return Response(serializer.data)
