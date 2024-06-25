98c06032a8c420f26461527fb1ee35bd7e0d3f29
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-06-25 09:05:58 +0500

    add cat_detail

--- cats/views.py
+++ cats/views.py
@@ -17,3 +17,19 @@ def cat_list(request):
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def cat_detail(request, pk):
+    cat = Cat.objects.get(pk=pk)
+    if request.method == 'PUT' or request.method == 'PATCH':
+        serializer = CatSerializer(cat, data=request.data, partial = True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        cat.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
+    serializer = CatSerializer(cat)
+    return Response(serializer.data)
