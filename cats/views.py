5f08581daba5627e9dac04cfc838826a1577b1ee
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-06-25 16:32:25 +0500

    add ModelViewSet

--- cats/views.py
+++ cats/views.py
@@ -3,19 +3,26 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics
+from rest_framework import viewsets
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-class CatList(generics.ListCreateAPIView):
+class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
 
 
-class CatDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Cat.objects.all()
-    serializer_class = CatSerializer
+# And we dont need it more also
+# class CatList(generics.ListCreateAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+
+
+# class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
 
 
 # No need that anymore either
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 # from cats.views import cat_list, cat_detail
 # from cats.views import APICat
-from cats.views import CatList, CatDetail
+# from cats.views import CatList, CatDetail
+from cats.views import CatViewSet
 
 
 urlpatterns = [
    # path('cats/', cat_list),
    # path('cats/', APICat.as_view()),
    # path('cats/<int:pk>/', cat_detail),
-   path('cats/', CatList.as_view()),
-   path('cats/<int:pk>/', CatDetail.as_view()),
+   # path('cats/', CatList.as_view()),
+   # path('cats/<int:pk>/', CatDetail.as_view()),
 ]
